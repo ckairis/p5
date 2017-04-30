d45cba6af47ad07579e8c665da7d2c239f09a91f
ckairis <kairis@wisc.edu>
2017-04-29 19:25:14 -0500

    Update NavigationGraph.java

--- NavigationGraph.java
+++ NavigationGraph.java
@@ -1,52 +1,95 @@
+import java.util.ArrayList;
 import java.util.List;
-//boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 
 public class NavigationGraph implements GraphADT<Location, Path> {
 
+	//List of locations
+	private List<Location> locations = new ArrayList<Location>();
+	
 	//TODO: Implement all methods of GraphADT
+	private List<GraphNode<Location,Path>> predecessor = new 
+			ArrayList<GraphNode<Location,Path>>();
+	
+	//List of GraphNodes
+	private List<GraphNode<Location,Path>> nodes = new 
+			ArrayList<GraphNode<Location,Path>>();
+	
+	//List of Edge Properties
+	private String[] edgePropertyNames;
+	
 	
 	public NavigationGraph(String[] edgePropertyNames) {
-		
+		this.edgePropertyNames = edgePropertyNames;
 	}
 
 	@Override
-	public void addVertex(V vertex) {
-		
+	public void addVertex(Location vertex) {
+		//add new Location to list, use index in list as ID
+		this.nodes.add(new GraphNode<Location,Path>(vertex, nodes.size()));
 	}
 	
 	@Override
-	public void addEdge(V src, V dest, E edge) {
-		
+	public void addEdge(Location src, Location dest, Path edge) {
+		for (int i= 0; i < nodes.size(); i++) {
+			if (nodes.get(i).getVertexData().equals(src)) {
+				
+				//Add out edge to source's edge list
+				nodes.get(i).addOutEdge(edge);
+			}
+		}
 	}
 	
 	@Override
-	public List<V> getVertices() {
+	public List<Location> getVertices() {
+		for (int i=0; i < nodes.size(); i++) {
+			locations.add(nodes.get(i).getVertexData());
+		}
+		return locations;
+	}
+	
+	@Override
+	public Path getEdgeIfExists(Location src, Location dest) {
+		
+		GraphNode<Location,Path> temp = null;
+		
+		for (int i=0; i < nodes.size(); i++) {
+			if (nodes.get(i).getVertexData().equals(src)) {
+				temp = nodes.get(i);
+			}
+		}
+		for (int j = 0; j < temp.getOutEdges().size(); j++) {
+			if (temp.getOutEdges().get(j).getDestination().equals(dest)) {
+				return temp.getOutEdges().get(j);
+			}
+		}
 		return null;
 	}
 	
 	@Override
-	public E getEdgeIfExists(V src, V dest) {
-		
+	public List<Path> getOutEdges(Location src) {
+			List<Path> temp = null;
+		for (int i= 0; i < nodes.size(); i++) {
+			if (nodes.get(i).getVertexData().equals(src)) {
+				temp = nodes.get(i).getOutEdges();
+			}
+		}	
+		return temp;
 	}
 	
 	@Override
-	public List<E> getOutEdges(V src) {
-		
+	public List<Location> getNeighbors(Location vertex) {
+		return null;
 	}
 	
 	@Override
-	public List<V> getNeighbors(V vertex) {
-		
-	}
-	
-	@Override
-	public List<E> getShortestRoute(V src, V dest, String edgePropertyName) {
-		
+	public List<Path> getShortestRoute(Location src, Location dest,
+			String edgePropertyName) {
+		return null;
 	}
 	
 	@Override
 	public String[] getEdgePropertyNames() {
-		return null;
+		return this.edgePropertyNames;
 	}
 	
 	@Override
@@ -61,6 +104,8 @@ public class NavigationGraph implements GraphADT<Location, Path> {
 	 * @return Location object
 	 */
 	public Location getLocationByName(String name) {
+		
+		
 		return null; //TODO: implement correctly. 
 	}
 
